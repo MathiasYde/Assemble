3367eb9225a35f2cbb134f9d1a7101db8691938a
Mathias Ransgaard Yde <mathias.yde@hotmail.dk>
2024-05-31 16:45:58 +0200

    add basic particles and sounds to heater block

--- src/main/java/com/example/PeaumarMod/Blocks/HeaterBlock.java
+++ src/main/java/com/example/PeaumarMod/Blocks/HeaterBlock.java
@@ -4,11 +4,14 @@ import com.example.PeaumarMod.PeaumarMod;
 import com.example.PeaumarMod.Utils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
@@ -70,9 +73,22 @@ public class HeaterBlock extends Block implements EntityBlock {
     protected @NotNull ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
         // TODO(mathias): this should probably be more sophisticated
         // TODO like holding shift to input the entire stack?
-        
+
         int burnTime = Utils.getBurnTime(stack.getItem());
-        if (!level.isClientSide && burnTime > 0) {
+        if (burnTime <= 0) {
+            return ItemInteractionResult.FAIL;
+        }
+
+        if (level.isClientSide) {
+            level.playLocalSound(
+                    pos,
+                    SoundEvents.FIRECHARGE_USE,
+                    SoundSource.BLOCKS,
+                    0.4f, 1.0f, false
+            );
+        }
+
+        if (!level.isClientSide) {
             BlockEntity blockEntity = level.getBlockEntity(pos);
             if (blockEntity instanceof HeaterBlockEntity heaterBlockEntity) {
                 heaterBlockEntity.addFuel(burnTime);
@@ -88,6 +104,35 @@ public class HeaterBlock extends Block implements EntityBlock {
         }
 
         return ItemInteractionResult.FAIL;
+    }
+
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+        if (state.getValue(LIT_STATE_PROPERTY) != LitState.OFF) {
+            double x = pos.getX() + 0.5;
+            double y = pos.getY() + 0.5;
+            double z = pos.getZ() + 0.5;
+
+            if (random.nextInt(10) == 0) {
+                level.playLocalSound(
+                        x, y, z,
+                        SoundEvents.CAMPFIRE_CRACKLE,
+                        SoundSource.BLOCKS,
+                        0.5F + random.nextFloat(),
+                        random.nextFloat() * 0.7F + 0.6F,
+                        false
+                );
+            }
+
+            Direction direction = state.getValue(FACING_PROPERTY);
+            Direction.Axis axis = direction.getAxis();
+            double factor = 0.52;
+            double offset = random.nextDouble() * 0.6 - 0.3;
+            double xOffset = axis == Direction.Axis.X ? direction.getStepX() * factor : offset;
+            double zOffset = random.nextDouble() * 6.0 / 16.0;
+            double yOffset = axis == Direction.Axis.Z ? direction.getStepZ() * factor : offset;
+            level.addParticle(ParticleTypes.SMOKE, x + xOffset, y + yOffset, z + zOffset, 0.0, 0.0, 0.0);
+        }
     }
 
     @Nullable
