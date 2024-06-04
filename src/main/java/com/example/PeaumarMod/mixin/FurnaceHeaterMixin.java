04b03de8e1179d823054bdf3e0e802f2a9a73d17
Mathias Ransgaard Yde <mathias.yde@hotmail.dk>
2024-06-04 20:11:15 +0200

    make furnace speed depend on below heater state

--- src/main/java/com/example/PeaumarMod/mixin/FurnaceHeaterMixin.java
+++ src/main/java/com/example/PeaumarMod/mixin/FurnaceHeaterMixin.java
@@ -3,13 +3,16 @@ package com.example.PeaumarMod.mixin;
 import com.example.PeaumarMod.Blocks.HeaterBlock;
 import com.example.PeaumarMod.Blocks.LitState;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Container;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FurnaceBlock;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,20 +22,34 @@ public class FurnaceHeaterMixin {
     private static class AbstractFurnaceBlockEntityMixin {
 
         @Inject(method="getTotalCookTime", at=@At("RETURN"), cancellable = true)
-        private static void injected(CallbackInfoReturnable<Integer> info) {
-            info.setReturnValue(10);
-        }
-    }
+        private static void modifyTotalCookTime(Level level, AbstractFurnaceBlockEntity entity, CallbackInfoReturnable<Integer> info) {
+            int ticks = info.getReturnValue();
 
-    @Mixin(FurnaceBlock.class)
-    public static class FurnaceBlockMixin {
-        @Unique
-        public void neighborChanged(BlockState state, Level world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
-            BlockState blockStateBelow = world.getBlockState(pos.below());
+            BlockState blockStateBelow = level.getBlockState(entity.getBlockPos().below());
+            System.out.println("Got BlockState: " + blockStateBelow.getBlock());
             if (blockStateBelow.getBlock() instanceof HeaterBlock heaterBlock) {
                 LitState litState = blockStateBelow.getValue(HeaterBlock.LIT_STATE_PROPERTY);
-                System.out.println("Furnace: Heater below is " + litState.getSerializedName() + "!");
+                System.out.println("Got LitState: " + litState);
+
+                System.out.println("Ticks before: " + ticks);
+                // this was bad :(
+//                ticks = switch (litState) {
+//                    case OFF -> ticks;
+//                    case FIRE -> ticks / 2;
+//                    case SOUL -> ticks / 4;
+//                };
+
+                // this is fine ??
+                if (litState == LitState.FIRE) {
+                    ticks = ticks / 2;
+                } else if (litState == LitState.SOUL) {
+                    ticks = ticks / 4;
+                }
+
+                System.out.println("Ticks after: " + ticks);
             }
+
+            info.setReturnValue(ticks);
         }
     }
 }
