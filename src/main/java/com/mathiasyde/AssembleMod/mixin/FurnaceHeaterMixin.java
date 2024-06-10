904c53fee2746f591947b52df31fcac9378a4679
Mathias Ransgaard Yde <mathias.yde@hotmail.dk>
2024-06-10 22:23:58 +0200

    Clean up

--- src/main/java/com/mathiasyde/AssembleMod/mixin/FurnaceHeaterMixin.java
+++ src/main/java/com/mathiasyde/AssembleMod/mixin/FurnaceHeaterMixin.java
@@ -1,18 +1,11 @@
 package com.mathiasyde.AssembleMod.mixin;
 
 import com.mathiasyde.AssembleMod.Blocks.HeaterBlock;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.Container;
-import net.minecraft.world.item.crafting.AbstractCookingRecipe;
-import net.minecraft.world.item.crafting.RecipeManager;
 import com.mathiasyde.AssembleMod.Datamodels.LitState;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.FurnaceBlock;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
