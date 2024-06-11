dc18339f3e74df3f0a425f217c331292778f2f69
Mathias Ransgaard Yde <mathias.yde@hotmail.dk>
2024-06-11 20:11:41 +0200

    add bronze barrel block

---
+++ src/main/java/com/mathiasyde/AssembleMod/Blocks/BronzeBarrel.java
@@ -0,0 +1,10 @@
+package com.mathiasyde.AssembleMod.Blocks;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public class BronzeBarrel extends Block {
+    public BronzeBarrel() {
+        super(BlockBehaviour.Properties.of());
+    }
+}
