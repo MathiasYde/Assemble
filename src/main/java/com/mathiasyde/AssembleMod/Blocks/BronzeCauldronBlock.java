f4942a8c805afbfd92af1be159fec945fec5adbd
Mathias <mathias.yde@hotmail.dk>
2024-07-04 14:23:30 +0200

    make cauldrons containers and suck in items, make mineable by iron pickaxe

--- src/main/java/com/mathiasyde/AssembleMod/Blocks/BronzeCauldronBlock.java
+++ src/main/java/com/mathiasyde/AssembleMod/Blocks/BronzeCauldronBlock.java
@@ -2,6 +2,7 @@ package com.mathiasyde.AssembleMod.Blocks;
 
 import com.mathiasyde.AssembleMod.AssembleMod;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -13,13 +14,15 @@ import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BronzeCauldronBlock extends Block implements EntityBlock {
     public static final String NAME = "bronze_cauldron";
@@ -27,7 +30,11 @@ public class BronzeCauldronBlock extends Block implements EntityBlock {
     public static final VoxelShape SUCK = Block.box(4, 4, 4, 12, 12, 12);
 
     public BronzeCauldronBlock() {
-        super(BlockBehaviour.Properties.of().noOcclusion());
+        super(BlockBehaviour.Properties.of()
+                .noOcclusion()
+                .requiresCorrectToolForDrops()
+                .strength(1.2F, 4.0F)
+        );
     }
 
     @Override
@@ -39,6 +46,24 @@ public class BronzeCauldronBlock extends Block implements EntityBlock {
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
         ItemStack itemStack = player.getItemInHand(hand);
         return InteractionResult.PASS;
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState pState, LootParams.Builder pParams) {
+        List<ItemStack> drops = super.getDrops(pState, pParams);
+        drops.add(new ItemStack(this));
+        return drops;
+    }
+
+    @Override
+    public void onRemove(BlockState oldState, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        if (oldState.is(newState.getBlock()) == false) {
+            if (level.getBlockEntity(pos) instanceof BronzeCauldronBlockEntity cauldronBlockEntity) {
+                Containers.dropContents(level, pos, cauldronBlockEntity);
+            }
+
+            super.onRemove(oldState, level, pos, newState, movedByPiston);
+        }
     }
 
     @Nullable
--- src/main/java/com/mathiasyde/AssembleMod/Blocks/BronzeCauldronBlockEntity.java
+++ src/main/java/com/mathiasyde/AssembleMod/Blocks/BronzeCauldronBlockEntity.java
@@ -2,25 +2,36 @@ package com.mathiasyde.AssembleMod.Blocks;
 
 import com.mathiasyde.AssembleMod.AssembleMod;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.Container;
+import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.fluids.FluidStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class BronzeCauldronBlockEntity extends BlockEntity {
+public class BronzeCauldronBlockEntity extends BlockEntity implements Container {
+    private NonNullList<ItemStack> items = NonNullList.withSize(8, ItemStack.EMPTY);
+
     public BronzeCauldronBlockEntity(BlockPos pos, BlockState state) {
         super(AssembleMod.BRONZE_CAULDRON_BLOCK_ENTITY.get(), pos, state);
     }
 
     public static <T extends BlockEntity> void tick(Level level, BlockPos pos, BlockState state, T entity) {
+        // TODO(mathias): add a transfer cooldown, just like a hopper
         if (entity instanceof BronzeCauldronBlockEntity cauldronBlockEntity) {
             List<ItemEntity> itemEntities = getItemsToSuckInFromAbove(level, pos);
+            for (ItemEntity itemEntity : itemEntities) {
+                HopperBlockEntity.addItem(cauldronBlockEntity, itemEntity);
+            }
         }
     }
 
@@ -31,5 +42,48 @@ public class BronzeCauldronBlockEntity extends BlockEntity {
                         aabb.move(pos),
                         EntitySelector.ENTITY_STILL_ALIVE).stream()
             ).collect(Collectors.toList());
+    }
+
+    @Override
+    public int getContainerSize() {
+        return items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    @Override
+    public @NotNull ItemStack getItem(int slot) {
+        return items.get(slot);
+    }
+
+    @Override
+    public @NotNull ItemStack removeItem(int slot, int quantity) {
+        return ContainerHelper.removeItem(items, slot, quantity);
+    }
+
+    @Override
+    public @NotNull ItemStack removeItemNoUpdate(int slot) {
+        return ContainerHelper.takeItem(this.items, slot);
+    }
+
+    @Override
+    public void setItem(int slot, ItemStack itemStack) {
+        this.items.set(slot, itemStack);
+        if (itemStack.getCount() > this.getMaxStackSize()) {
+            itemStack.setCount(this.getMaxStackSize());
+        }
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return Container.stillValidBlockEntity(this, player);
+    }
+
+    @Override
+    public void clearContent() {
+        this.items.clear();
     }
 }
